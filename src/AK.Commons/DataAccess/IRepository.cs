7f0fd9bb12fff0c637c5e7c28647d6f61f428fd8
Aashish Koirala <aashish.koirala@radiusww.com>
2016-04-08 16:58:27 -0400

    WIP

--- src/AK.Commons/DataAccess/IRepository.cs
+++ src/AK.Commons/DataAccess/IRepository.cs
@@ -19,7 +19,11 @@
  * 
  *******************************************************************************************************************************/
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace AK.Commons.DataAccess
 {
@@ -38,12 +42,10 @@ namespace AK.Commons.DataAccess
     /// <author>Aashish Koirala</author>
     public interface IRepository<T> : IRepository where T : class
     {
-        T Create<TKey>(TKey id) where TKey : struct;
+        T GetOrCreate<TKey>(TKey id, Func<TKey, T> creator = null) where TKey : struct;
 
-        T Get<TKey>(TKey id) where TKey : struct;
+        IEnumerable<T> GetOrCreateList<TKey>(IEnumerable<TKey> idList, Func<TKey, T> creator = null) where TKey : struct;
 
-        T GetOrCreate<TKey>(TKey id) where TKey : struct;
-        
         /// <summary>
         /// A LINQ-queryable interface for data access to the repository.
         /// </summary>
@@ -60,5 +62,157 @@ namespace AK.Commons.DataAccess
         /// </summary>
         /// <param name="thing">Entity to delete.</param>
         void Delete(T thing);
+    }
+
+    public static class RepositoryMappingExtensions
+    {
+        public static TOut Map<TIn, TOut, TKey>(this IRepository<TOut> repository,
+            TIn inObject,
+            Action<ObjectMapper<TIn, TOut>> mapper = null,
+            Func<TKey, TOut> creator = null,
+            Expression<Func<TIn, TKey>> keyExpression = null)
+            where TIn : class
+            where TOut : class
+            where TKey : struct
+        {
+            if (keyExpression == null)
+            {
+                var targetExpression = Expression.Parameter(typeof (TIn), "x");
+                var idAccessExpression = Expression.PropertyOrField(targetExpression, "Id");
+                keyExpression = (Expression<Func<TIn, TKey>>) Expression.Lambda(idAccessExpression, targetExpression);
+            }
+
+            var key = keyExpression.Compile()(inObject);
+            var outObject = repository.GetOrCreate(key, creator);
+            var objectMapper = new ObjectMapper<TIn, TOut>(inObject, outObject);
+            if (mapper == null) objectMapper.AutoMap();
+            else mapper(objectMapper);
+            return outObject;
+        }
+
+        public static void MapAndSave<TIn, TOut, TKey>(this IRepository<TOut> repository,
+            TIn inObject,
+            Action<ObjectMapper<TIn, TOut>> mapper = null,
+            Func<TKey, TOut> creator = null,
+            Expression<Func<TIn, TKey>> keyExpression = null)
+            where TIn : class
+            where TOut : class
+            where TKey : struct
+        {
+            var thing = repository.Map(inObject, mapper, creator, keyExpression);
+            repository.Save(thing);
+        }
+
+        public static IEnumerable<TOut> MapList<TIn, TOut, TKey>(this IRepository<TOut> repository,
+            IEnumerable<TIn> inObjects,
+            IEnumerable<TOut> existingOutObjects = null,
+            Action<ObjectMapper<TIn, TOut>> mapper = null,
+            Func<TKey, TOut> creator = null,
+            Expression<Func<TIn, TKey>> inKeyExpression = null,
+            Expression<Func<TOut, TKey>> outKeyExpression = null)
+            where TIn : class
+            where TOut : class
+            where TKey : struct
+        {
+            if (inKeyExpression == null)
+            {
+                var targetExpression = Expression.Parameter(typeof (TIn), "x");
+                var idAccessExpression = Expression.PropertyOrField(targetExpression, "Id");
+                inKeyExpression = (Expression<Func<TIn, TKey>>) Expression.Lambda(idAccessExpression, targetExpression);
+            }
+
+            if (outKeyExpression == null)
+            {
+                var targetExpression = Expression.Parameter(typeof (TIn), "x");
+                var idAccessExpression = Expression.PropertyOrField(targetExpression, "Id");
+                outKeyExpression = (Expression<Func<TOut, TKey>>) Expression.Lambda(idAccessExpression, targetExpression);
+            }
+
+            var inKeyFunc = inKeyExpression.Compile();
+            var outKeyFunc = outKeyExpression.Compile();
+            var keyList = inObjects.Select(inKeyFunc).ToArray();
+            var outObjects = repository.GetOrCreateList(keyList, creator);
+            outObjects.ForEach(x =>
+            {
+                var outKey = outKeyFunc(x);
+                var inObject = inObjects.Single(y => inKeyFunc(y).Equals(outKey));
+                var objectMapper = new ObjectMapper<TIn, TOut>(inObject, x);
+                if (mapper == null) objectMapper.AutoMap();
+                else mapper(objectMapper);
+            });
+
+            existingOutObjects = existingOutObjects ?? new TOut[0];
+            if (!existingOutObjects.Any()) return outObjects;
+
+            var objectsToRemove = existingOutObjects.Where(x => !keyList.Contains(outKeyFunc(x))).ToArray();
+            foreach (var objectToRemove in objectsToRemove) repository.Delete(objectToRemove);
+
+            return outObjects;
+        }
+
+        public static void MapAndSaveList<TIn, TOut, TKey>(this IRepository<TOut> repository,
+            IEnumerable<TIn> inObjects,
+            IEnumerable<TOut> existingOutObjects = null,
+            Action<ObjectMapper<TIn, TOut>> mapper = null,
+            Func<TKey, TOut> creator = null,
+            Expression<Func<TIn, TKey>> inKeyExpression = null,
+            Expression<Func<TOut, TKey>> outKeyExpression = null)
+            where TIn : class
+            where TOut : class
+            where TKey : struct
+        {
+            var list = repository.MapList(inObjects, existingOutObjects, mapper, creator, inKeyExpression, outKeyExpression);
+            list.ForEach(repository.Save);
+        }
+    }
+
+    public class ObjectMapper<TSource, TTarget> where TSource : class where TTarget : class
+    {
+        public ObjectMapper(TSource source, TTarget target)
+        {
+            this.Source = source;
+            this.Target = target;
+        }
+
+        public TSource Source { get; }
+        public TTarget Target { get; }
+
+        public AfterAutoMap<TSource, TTarget> AutoMap()
+        {
+            var sourceProperties = typeof (TSource)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => x.CanRead);
+
+            foreach (var sourceProperty in sourceProperties)
+            {
+                var targetProperty = typeof (TTarget)
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .SingleOrDefault(x => x.CanWrite && x.Name == sourceProperty.Name && x.PropertyType == sourceProperty.PropertyType);
+
+                if (targetProperty == null) continue;
+
+                var sourceValue = sourceProperty.GetValue(this.Source);
+                targetProperty.SetValue(this.Target, sourceValue);
+            }
+
+            return new AfterAutoMap<TSource, TTarget>(this.Source, this.Target);
+        }
+    }
+
+    public class AfterAutoMap<TSource, TTarget> where TSource : class where TTarget : class
+    {
+        private readonly TSource source;
+        private readonly TTarget target;
+
+        public AfterAutoMap(TSource source, TTarget target)
+        {
+            this.source = source;
+            this.target = target;
+        }
+
+        public void Then(Action<TSource, TTarget> mapper)
+        {
+            mapper(this.source, this.target);
+        }
     }
 }
